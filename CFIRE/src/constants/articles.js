b0152251ad10d64020cfce64c3c210edeb534776
lordnine <74161592+lordnine@users.noreply.github.com>
2020-12-29 19:41:51 -0500

    Add files via upload

--- CFIRE/src/constants/articles.js
+++ CFIRE/src/constants/articles.js
@@ -1,37 +1,61 @@
-import muskian from '../assets/images/muskian.jpg';
+import ajHastings from '../assets/images/aj-hastings.jpg';
+import johnnys from '../assets/images/johnnys.jpg';
+import chipotle from '../assets/images/chipotle.jpg';
+import hair_east from '../assets/images/haireast.jpg';
+import ll_bean from '../assets/images/llbean.jpg';
+import donnies from '../assets/images/donnies.jpg';
 
 export default [
     {
       key: 0,
-      title: 'Enjoy 10% off of your purchase with your student ID',
-      img: muskian,
-      cta: 'View Deal', 
+      companyName: 'AJ Hastings',
+      title: 'Enjoy 10% off with QR Code',
+      img: ajHastings,
       horizontal: true,
       full: false,
+      food: false,
+      cta: 'view shiz',
     },
     {
       key: 1,
-      title: 'Is makeup one of your daily esse …',
-      image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-      cta: 'View article'
+      companyName: "Johnny's Tavern",
+      img: johnnys,
+      title: 'Free Appetizer with Purchase of Two Entrees',
+      food: true,
+      cta: 'view stuff',
     },
     {
       key: 2,
-      title: 'Coffee is more than just a drink: It’s …',
-      image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
-      cta: 'View article' 
+      companyName: 'Chipotle',
+      title: 'Free side of guac with purchase of entree and drink',
+      img: chipotle,
+      cta: 'View Code',
+      food: true,
     },
     {
       key: 3,
-      title: 'Fashion is a popular style, especially in …',
-      image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-      cta: 'View article' 
+      companyName: 'LL Bean',
+      title: 'Holiday sweater sale',
+      img: ll_bean,
+      horizontal: true,
+      cta: 'View Deal',
+      food: false,
     },
     {
       key: 4,
-      title: 'Argon is a great free UI packag …',
-      image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+      companyName: "Donnie's Steak House",
+      title: 'Burger and fries',
+      img: donnies,
       cta: 'View article', 
-      horizontal: true
+      horizontal: true,
+      food: true,
     }, 
+    {
+      key: 5,
+      companyName: 'Hair East',
+      title: '$20 Terrible Haircut on Mondays',
+      img: hair_east,
+      cta: 'View Deal',
+      food: false,
+    }
   ];
