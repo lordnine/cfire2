41ca5cdced4b150421aa34318b641d1391cc37c1
lordnine <16plane29@gmail.com>
2021-01-08 23:04:47 -0500

    Add text styles file

---
+++ src/styles/text.js
@@ -0,0 +1,15 @@
+
+const text = {
+    
+    minimalistButtonText: {
+        fontWeight: 'bold', 
+        color: 'white'
+      },
+      inverseMinimalistButtonText: {
+        fontWeight: 'bold', 
+        color: '#000437',
+      },
+
+};
+
+export default text;
