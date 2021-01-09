a33bb6c994aad107313ad35c79bb691ac81a21f6
lordnine <16plane29@gmail.com>
2021-01-08 23:48:14 -0500

    Implement containers style sheet in welcome

--- src/screens/welcomeScreen/welcomeScreen.js
+++ src/screens/welcomeScreen/welcomeScreen.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {
-  Text,
   View,
   StyleSheet,
   Image
 } from 'react-native';
-import common from '../../styles/common';
 import logo from '../../assets/images/logo.png';
 import { Button, Divider} from 'react-native-elements';
 import buttons from '../../styles/buttons';
 import text from '../../styles/text';
 import customStyles from '../../styles/customStyles';
+import containers from '../../styles/containers';
 
 export default class WelcomeScreen extends React.Component {
     static navigationOptions = {
@@ -20,23 +19,28 @@ export default class WelcomeScreen extends React.Component {
 
     render() {
       return (
-        <View style={styles.frameContainer}>
+        <View style={containers.frameContainer}>
+
           <Divider style={customStyles.transparentDivider('10%')} />
           <Image style={{ width: 55, height: 55,}}  resizeMode='contain' source={logo}/>
           <Divider style={customStyles.transparentDivider('45%')} />
+
           <Button
               title="LEARN MORE"
               buttonStyle={buttons.minimalistButton}
               titleStyle={text.minimalistButtonText}
               onPress={() => this.props.navigation.navigate('CreateAccount')}
             />
-           <Divider style={{ height: 18, backgroundColor: 'transparent' }} />
+
+          <Divider style={customStyles.transparentDivider('3%')} />
+
           <Button
               title="GET STARTED"
               buttonStyle={buttons.inverseMinimalistButton}
               titleStyle={text.inverseMinimalistButtonText}
               onPress={() => this.props.navigation.navigate('Home')}
             />
+
         </View>
       );
     }
@@ -44,30 +48,6 @@ export default class WelcomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
 
-  text: {
-    fontSize: 38,
-    color: 'white',
-    fontWeight: '400'
-  },
 
-  input: common.minimalistInput,
-  button: common.minimalistButton,
-  textContainer: {
-    alignItems: 'center',
-  },
-
-  titleText: common.titleText,
-  text: {
-    ...common.text,
-    ...common.textLink,
-  },
-
-  megaText: common.megaText,
-  
-  subtitleText: common.subtitleText,
-
-  inverseButton: common.inverseMinimalistButton,
-
-  frameContainer: common.redContainer
 });
 
