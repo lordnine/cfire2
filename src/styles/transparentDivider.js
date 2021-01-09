ecb7ac4f2ef936dfb65fe0d3f971d129a005e203
lordnine <16plane29@gmail.com>
2021-01-08 23:18:46 -0500

    Add transparent divider style function

---
+++ src/styles/transparentDivider.js
@@ -0,0 +1,12 @@
+
+
+const transparentDivider = (y) => {
+        return{
+            height: y,
+            backgroundColor: 'transparent',
+    
+        };
+    };
+
+    
+export default transparentDivider;
