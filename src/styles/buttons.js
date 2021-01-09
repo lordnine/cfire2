97fec95e9b7da6e74d7c763b763cf403c5e13106
lordnine <16plane29@gmail.com>
2021-01-08 22:58:35 -0500

    Add buttons style sheet

---
+++ src/styles/buttons.js
@@ -0,0 +1,22 @@
+
+const buttons = {
+    minimalistButton: {
+        height: 50,
+        width: 280,
+        backgroundColor: 'transparent',
+        borderWidth: 2,
+        borderColor: 'white',
+        borderRadius: 30,
+      },
+      inverseMinimalistButton: {
+        height: 50,
+        width: 280,
+        backgroundColor: 'white',
+        borderWidth: 2,
+        borderColor: 'white',
+        borderRadius: 30,
+      },
+
+};
+
+export default buttons;
