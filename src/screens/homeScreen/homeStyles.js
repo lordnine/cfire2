77535d37289d5357ad58e84dff3b23ac41bacfff
lordnine <16plane29@gmail.com>
2021-01-11 18:17:22 -0500

    Add stylesheet to home folder

---
+++ src/screens/homeScreen/homeStyles.js
@@ -0,0 +1,22 @@
+
+
+
+const homeStyles = {
+
+    articleContainer: {
+        width: '48%',
+        padding: '0%',
+        marginHorizontal: '1%',
+        marginBottom: '0%'
+    },
+
+
+};
+
+
+
+
+
+
+
+export default homeStyles;
