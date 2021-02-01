345a2a6aaa904f387d55df688f1ad9ca11ffa95b
lordnine <16plane29@gmail.com>
2021-02-01 14:23:46 -0500

    Add style files for navigators

---
+++ src/navigation/appNavigator/appNavigatorStyles.js
@@ -0,0 +1,10 @@
+
+
+const appNavigatorStyles = {
+
+
+};
+
+
+
+export default appNavigatorStyles;
---
+++ src/navigation/appNavigator/drawerNavigator/drawerNavigatorStyles.js
@@ -0,0 +1,10 @@
+
+
+const drawerNavigatorStyles = {
+
+    
+
+};
+
+
+export default drawerNavigatorStyles;
