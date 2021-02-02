c70fcd7733c797652120bee7765e2afaa9e737f6
lordnine <16plane29@gmail.com>
2021-02-01 23:03:43 -0500

    Add search to drawer nav

--- src/navigation/appNavigator/drawerNavigator/drawerNavigator.js
+++ src/navigation/appNavigator/drawerNavigator/drawerNavigator.js
@@ -3,15 +3,17 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../../../screens/homeScreen';
 import SettingsScreen from '../../../screens/settingsScreen';
+import SearchScreen from '../../../screens/searchScreen'
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
 return(
-<Drawer.Navigator initialRouteName="Home" drawerPosition='right'>
-<Drawer.Screen name="Home" component={HomeScreen} />
-<Drawer.Screen name="Settings" component={SettingsScreen} />
-</Drawer.Navigator>
+    <Drawer.Navigator initialRouteName="Home" drawerPosition='right'>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+        <Drawer.Screen name="Search" component={SearchScreen} />
+    </Drawer.Navigator>
 );
 
 }
