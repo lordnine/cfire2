228f1f598b627b5e34584ea0068858f1562b30c6
lordnine <16plane29@gmail.com>
2021-03-19 18:57:21 -0400

    cleanup tabNavigator

--- src/navigation/appNavigator/tabNavigator/tabNavigator.js
+++ src/navigation/appNavigator/tabNavigator/tabNavigator.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image } from 'react-native';
-import SearchScreen from '../../../screens/searchScreen';
 import CategoryScreen from '../../../screens/categoryScreen';
 import HomeScreen from '../../../screens/homeScreen';
 import houselogo from '../../../assets/images/houselogo.png';
 import searchlogo from '../../../assets/images/searchlogo.png';
+
+import tabNavigatorStyles from './tabNavigatorStyles';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,28 +16,25 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
 return(
     <Tab.Navigator initialRouteName="Home" drawerPosition='right' 
-    tabBarOptions={{
-      activeTintColor: 'black',
-      inactiveTintColor: 'darkgrey',
-    }} 
+    tabBarOptions={tabNavigatorStyles.tintStyle} 
    >
         <Tab.Screen name="Home" component={HomeScreen} 
         options={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             // You can return any component that you like here!
             return (
-            <Image source={houselogo} style={{height: 16, width: 16, marginTop: 8, tintColor: color }} resizeMode='contain'/>
+            <Image source={houselogo} style={[tabNavigatorStyles.tabLogo, {tintColor: color }]} resizeMode='contain'/>
              );
              
-          },
-        })}
+            },
+            })}
         />
         <Tab.Screen name="Search" component={CategoryScreen} 
                 options={({ route }) => ({
                   tabBarIcon: ({ focused, color, size }) => {
                     // You can return any component that you like here!
                     return (
-                    <Image source={searchlogo} style={{height: 16, width: 16, marginTop: 8, tintColor: color }} resizeMode='contain' />
+                    <Image source={searchlogo} style={[tabNavigatorStyles.tabLogo, {tintColor: color }]} resizeMode='contain' />
                      );
                   },
                 })}
